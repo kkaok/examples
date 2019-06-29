63010127415e9ab8589a3066f2be39f36ab35f43
june <june@DESKTOP-5CJLJAA>
2019-06-29 21:44:11 +0900

    SheetJs 엑셀 읽기 예제

---
+++ jsExcelExample/src/main/java/eblo/example/jsexcel/JsExcelExampleApplication.java
@@ -0,0 +1,13 @@
+package eblo.example.jsexcel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsExcelExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsExcelExampleApplication.class, args);
+	}
+
+}
